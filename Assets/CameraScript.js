60bc202de28c9309c0f62be806d07eeef4f21ca9
MihneaS <mihnea.serban@inflop.ro>
2015-03-03 01:49:42 +0200

    repared camera bug + now, camera can't exit the map

--- Assets/CameraScript.js
+++ Assets/CameraScript.js
@@ -9,17 +9,20 @@ var lockKey : KeyCode;
 var lookAtChar : KeyCode;
 
 var locked : boolean = false;
+var intermediate_position : Vector3;
 
 var speed : float = 20;
 var FRACTION : float = 0.95; // cand cursorul depaseste fractie*inaltimea_ecranului, camera merge in sus si pt jos (1-fractie)*inaltimea
 
 var characterToFollow : Transform;
+var map : SpriteRenderer;
 var difference : Vector3;
 
+private var max : Vector3;
+private var min : Vector3;
+
 function classic_movement ()
-{
-	rigidbody2D.velocity = Vector2.zero;
-	
+{	
 	if (Input.GetKey(moveUp))
 	{
 		transform.Translate(Vector3.up*Time.deltaTime*speed,Space.World);
@@ -50,9 +53,43 @@ function moving_with_cursor_at_edge()
 		transform.Translate(Vector3.left*Time.deltaTime*speed,Space.World);
 }
 
+function movement_final_form_01()
+{
+	if (Input.GetKey(moveUp)||Input.mousePosition.y > Screen.height * FRACTION)
+		transform.Translate(Vector3.up*Time.deltaTime*speed,Space.World);
+	if (Input.GetKey(moveDown)||Input.mousePosition.y < Screen.height * (1-FRACTION))
+		transform.Translate(Vector3.down*Time.deltaTime*speed,Space.World);
+	if(Input.GetKey(moveRight)||Input.mousePosition.x > Screen.width * FRACTION)
+		transform.Translate(Vector3.right*Time.deltaTime*speed,Space.World);
+	if(Input.GetKey(moveLeft)||Input.mousePosition.x < Screen.width * (1-FRACTION))
+		transform.Translate(Vector3.left*Time.deltaTime*speed,Space.World);
+}
+
+function movement_final_form_02()
+{
+	if (Input.GetKey(moveUp)||Input.mousePosition.y > Screen.height * FRACTION)
+		intermediate_position.y += speed*Time.deltaTime;
+	if (Input.GetKey(moveDown)||Input.mousePosition.y < Screen.height * (1-FRACTION))
+		intermediate_position.y += speed*Time.deltaTime*-1;
+	if(Input.GetKey(moveRight)||Input.mousePosition.x > Screen.width * FRACTION)
+		intermediate_position.x += speed*Time.deltaTime;
+	if(Input.GetKey(moveLeft)||Input.mousePosition.x < Screen.width * (1-FRACTION))
+		intermediate_position.x += speed*Time.deltaTime*-1;
+}
+
 function Start ()
 {
-	transform.Translate(Vector3.left*Time.deltaTime*speed,Space.World);
+	var vertExtent = Camera.main.camera.orthographicSize;    
+    var horzExtent = vertExtent * Screen.width / Screen.height;
+    
+    // Calculations assume map is position at the origin
+    min.x = horzExtent - map.sprite.bounds.extents.x;
+    max.x = map.sprite.bounds.extents.x - horzExtent;
+    min.y = horzExtent - map.sprite.bounds.extents.y;
+    max.y = map.sprite.bounds.extents.y - horzExtent;
+    
+    Debug.Log(max);
+    Debug.Log(min);
 }
 
 function Update ()
@@ -71,17 +108,18 @@ function Update ()
 	}
 	if(locked)
 	{
-		transform.position = characterToFollow.position + difference;
+		intermediate_position = characterToFollow.position + difference;
 	}
 	else
 	{
-		classic_movement();
-		moving_with_cursor_at_edge();
+		movement_final_form_02();
 	}
 	if(Input.GetKey(lookAtChar))
 	{
 		difference = Vector3.zero;
 		difference.z = transform.position.z - characterToFollow.position.z;
-		transform.position = characterToFollow.position + difference;
+		intermediate_position = characterToFollow.position + difference;
 	}
+	transform.position.x = Mathf.Clamp(intermediate_position.x, min.x, max.x);
+	transform.position.y = Mathf.Clamp(intermediate_position.y, min.y, max.y);
 }
