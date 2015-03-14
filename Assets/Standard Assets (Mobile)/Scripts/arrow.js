bffd3dd54156e4e391c9c5f2c7ea3ea0d09a88f4
Teodor Szente <teodor98sz@gmail.com>
2015-03-14 13:38:33 +0200

    clean

--- Assets/Standard Assets (Mobile)/Scripts/arrow.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict 
- 
- var speed : float = 1.0;
- 
- function Update() {
-
-     var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-     transform.position += -1*move * speed * Time.deltaTime;
- }
